c4072e185fdc93e3ad36ba37d4644a3778215326
viniciuspantoja-ai <vinicius@aiconsult.business>
2022-07-30 07:03:18 -0400

    moving pages to django_project folder

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("pages.urls")),
+    path("", include("django_project.pages.urls")),
 ]
