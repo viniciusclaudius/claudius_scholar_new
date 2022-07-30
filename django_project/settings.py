b2ec9f33f8070edd840968118013ad1f893bd3da
viniciuspantoja-ai <vinicius@aiconsult.business>
2022-07-30 07:06:56 -0400

    moving pages to django_project folder

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig',
+    'django_project.pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
